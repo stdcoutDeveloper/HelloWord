8cf00b32c8a4579060e5ec45229007136243e39b
stdcoutDeveloper <nhapmonvicodientu20152@gmail.com>
2020-07-26 11:00:20 +0700

    Classes

---
+++ HelloWorld/Foo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Foo
+    {
+        private byte userAge_ = 20;
+        private int numberOfEmployees_ = 200;
+
+        private double numberOfHours_ = 5120.5;
+        private float hourlyRate_ = 60.5F;
+        private decimal money_ = 10.0M;
+
+        private char grade_ = 'A';
+
+        private bool isPromoted_ = true;
+
+        public void DoFooThings()
+        {
+            int x = 5, y = 10;
+            x = y;
+            Console.WriteLine(x);
+
+            int z = (int) 20.9;
+        }
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,18 +1,23 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HelloWorld
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
+            
+
+            int x = 5, y = 10;
+            x = y;
+            Console.WriteLine(x + ", " + y);
+
+            Foo foo = new Foo();
+            foo.DoFooThings();
+
             Console.Read();
 
             // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
